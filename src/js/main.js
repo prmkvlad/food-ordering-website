6a38d031c3cc5b9e6a28c2db2cc46b7696b64e26
prmkvlad <prmk.vlad@gmail.com>
2023-08-26 19:10:27 +0300

    Reviews section style
    Connect swiper slider

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 2,
+		navigation: {
+			nextEl: '.reviews__arrow--next',
+			prevEl: '.reviews__arrow--prev',
+		},
+
+
+	});
+
+});
