0aa30e88fdef653d5d649a2ecd692741cfb8c4be
prmkvlad <prmk.vlad@gmail.com>
2024-04-10 01:51:44 +0300

    Add smooth scroll

--- js/main.js
+++ js/main.js
@@ -30,3 +30,20 @@ if (burger && menu) {
 		body.classList.toggle('_lock');
 	})
 }
+
+/*=================smooth scroll=================*/
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+	anchor.addEventListener('click', event => {
+		event.preventDefault();
+
+		const blockID = anchor.getAttribute('href').substring(1);
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: "smooth",
+			block: "start"
+		})
+	})
+})
